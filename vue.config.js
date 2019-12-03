766a9fde0434a7f631df0e2c5f311fcc6758978b
anliny <294288551@qq.com>
2019-12-03 19:00:56 +0800

    登录

--- vue.config.js
+++ vue.config.js
@@ -39,13 +39,15 @@ module.exports = {
   // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
   //   devServer: {
   //     proxy: 'localhost:3000'
+  //   ws: true,
+
   //   }
   devServer: {
     proxy: {
       '/': {
         target: 'http://106.13.70.95:888',
         changeOrigin: true,
-        ws: true,
+        ws: false,
         pathRewrite: {
           '^/': ''
         }
